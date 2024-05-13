298bf4f7dc08a41045ca425fc8f29a14a78c1c20
remya <remyap126@gmail.com>
2024-05-13 15:06:15 -0500

    committing with hooks and cross browser

---
+++ starpalyer/src/test/java/manager/driver/DriverManager.java
@@ -0,0 +1,17 @@
+package manager.driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverManager {
+	
+	public static WebDriver driver;
+	
+	public static WebDriver getDriver() {
+		if(driver == null) {
+			driver = new ChromeDriver();
+		}
+		return driver;
+	}
+
+}
